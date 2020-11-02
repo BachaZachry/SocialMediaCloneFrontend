d5daa8a02fe36a2c4462af743a7d1d6e890aa1f2
Bacha Ilyes <47483260+BachaZachry@users.noreply.github.com>
2020-11-02 18:33:40 +0100

    Project Init

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import rootReducer from './../reducers/index';
 
 export default configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: rootReducer
 });
